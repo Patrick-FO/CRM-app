0e3c6a12d951ef9383d22ad593f7926fc5d29503
Patrick-FO <patrick.olausson@gmail.com>
2025-05-26 13:34:26 +0200

    Navigation part of app done for view model decoupling

--- app/src/main/java/com/example/crmapp/navigation/Screen.kt
+++ app/src/main/java/com/example/crmapp/navigation/Screen.kt
@@ -4,5 +4,7 @@ sealed class Screen(val route: String) {
     object LoginScreen: Screen("login_screen")
     object SignupScreen: Screen("signup_screen")
     object HomeScreen: Screen("home_screen")
-    object ContactScreen: Screen("contact_screen/{contactId}")
+    object ContactScreen: Screen("contact_screen/{contactId}") {
+        fun createRoute(contactId: Int) = "contact_screen/$contactId"
+    }
 }
